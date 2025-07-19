b35c4b50a30f1ef296e28255dd8f2e131d48f484
Thirafi Najwan <reverseon@outlook.com>
2025-07-19 16:15:22 +0000

    migrate to issuer annotations

--- cluster-utilities/generate-helm.sh
+++ cluster-utilities/generate-helm.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-CATEGORY=$1
-FOLDER=$2
-PATCH=$3
+FOLDER=$1
+PATCH=$2
 
-if [ -z "$CATEGORY" ] || [ -z "$FOLDER" ] || [ -z "$PATCH" ]; then
-    echo "Usage: $0 <category> <folder> <patch>"
+if [ ! -d "$SCRIPT_DIR/$FOLDER" ]; then
+    echo "Folder $SCRIPT_DIR/$FOLDER does not exist"
     exit 1
 fi
 
-if [ ! -d "$CATEGORY/$FOLDER" ]; then
-    echo "Folder $CATEGORY/$FOLDER does not exist"
-    exit 1
-fi
-
-if [ ! -d "$CATEGORY/$FOLDER/patches/$PATCH" ]; then
+if [ ! -d "$SCRIPT_DIR/$FOLDER/patches/$PATCH" ]; then
     echo "Patch $PATCH does not exist"
     exit 1
 fi
 
-helmfile -f $CATEGORY/$FOLDER/patches/$PATCH/helmfile.yaml template --include-crds > $CATEGORY/$FOLDER/patches/$PATCH/helm-$FOLDER.yaml
+helmfile -f $SCRIPT_DIR/$FOLDER/patches/$PATCH/helmfile.yaml template --include-crds > $SCRIPT_DIR/$FOLDER/patches/$PATCH/helm-$FOLDER.yaml
